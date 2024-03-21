2158496644cbbf122eb0099763fc298f75ba8b6c
vNikitosik <Bluangel130@gmail.com>
2024-03-21 21:52:03 +0700

    1 задача 5го шага выполнена

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,26 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.cli();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        System.out.println("Your choice: ");
+        int value = scanner.nextInt();
+        while (value != 0 && value !=1) {
+            System.out.println("Выберете между 0 и 1");
+            value = scanner.nextInt();
+        }
+            if (value == 1) {
+                System.out.println("Welcome to the Brain Games!");
+                Cli.cli();
+            } else if (value == 0) {
+                scanner.close();
+            }
+
     }
-}
+    }
+
